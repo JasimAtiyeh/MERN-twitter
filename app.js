3f397247fdb1d938f6ba43566b2464136862b0fa
Jasim Atiyeh <jasimatiyeh@gmail.com>
2019-10-20 13:08:02 -0500

    Create first routes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
-const port = process.env.PORT || 5000;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
+const app = express();
+const port = process.env.PORT || 5000;
 
 mongoose
 .connect(db, {
@@ -12,5 +15,10 @@ mongoose
 .then(() => console.log("Connected to MongoDB successfully"))
 .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Nijim"));
+app.get("/", (req, res) => res.send("Nijim")); // listening for get requests on the root '/' route
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
