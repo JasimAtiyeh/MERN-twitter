27a161bf20a39d4278b16e914c9200fbf7935d03
Jasim Atiyeh <jasimatiyeh@gmail.com>
2019-10-20 15:09:20 -0500

    Add user login and register

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 mongoose
-.connect(db, {
-  useNewUrlParser: true
-})
-.then(() => console.log("Connected to MongoDB successfully"))
-.catch(err => console.log(err));
+  .connect(db, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Nijim")); // listening for get requests on the root '/' route
+// app.get("/", (req, res) => res.send("Nijim")); // listening for get requests on the root '/' route
+app.use(passport.initialize());
+require('./config/passport')(passport);
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 app.use("/api/users", users);
